f52d064265e9a7920ba0b3d785e5db532425025f
kautilya <kautilya101001@gmail.com>
2023-03-24 14:18:02 +0530

    feat : universities displayed using mapbox

--- src/db/db.js
+++ src/db/db.js
@@ -1,22 +1,37 @@
+require("dotenv").config();
+const postgres = require('postgres');
 
-const Pool = require('pg').Pool
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
-  password: '#12kautilya',
-  port: 4321,
-})
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
 
-// const getval = 'select count(creddesc),creddesc,instnm from university group by instnm,creddesc;'
-const getCourse = 'SELECT DISTINCT CREDDESC FROM university';
-const getType = 'SELECT DISTINCT CONTROL FROM university';
+const URL = `postgres://kbhardwaj:Mhwuq7sc6CAb@ep-spring-lab-667624.ap-southeast-1.aws.neon.tech/neondb`
+
+const pool = postgres(URL, { ssl: 'require' });
+
+async function getPostgresVersion() {
+  const result = await pool`select instnm from insts;`;
+  console.log(result);
+}
+
+getPostgresVersion();
+
+// const Pool = require('pg').Pool
+// const pool = new Pool({
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'postgres',
+//   password: '#12kautilya',
+//   port: 4321,
+// })
+
+const getval = `select count(creddesc),creddesc,instnm from university group by instnm,creddesc;`
+const getCourse = `SELECT DISTINCT CREDDESC FROM university;`
+const getType = `SELECT DISTINCT CONTROL FROM university;`
 
 
 async function getinstnms(){
   try {
-    const res = await pool.query('SELECT DISTINCT INSTNM FROM UNIVERSITY ORDER BY INSTNM');
-    return res.rows;
+    const res = await pool`SELECT INSTNM FROM INSTS ORDER BY INSTNM;`
+    return res;
   } catch (err) {
     return console.log(err);
   }
@@ -24,8 +39,8 @@ async function getinstnms(){
 
 async function getAllcourses() {
   try {
-    const res = await pool.query(getCourse);
-    return res.rows;
+    const res = await pool`getCourse`;
+    return res ;
   } catch (err) {
     return console.log(err);
   }
@@ -33,8 +48,8 @@ async function getAllcourses() {
 
 async function getAllType() {
   try {
-    const res = await pool.query(getType);
-    return res.rows;
+    const res = await pool`getType`;
+    return res ;
   } catch (err) {
     return console.log(err);
   }
@@ -45,24 +60,24 @@ async function getunis(control,course){
 
   if(control != 'None' && course != 'None'){
     try {
-      const res2 = await pool.query('SELECT DISTINCT INSTNM FROM university WHERE CREDDESC = $1 AND CONTROL = $2', [course,control]);
-      return res2.rows;
+      const res2 = await pool`SELECT DISTINCT INSTNM FROM university WHERE CREDDESC = ${course} AND CONTROL = ${control};`
+      return res2 ;
     } catch (err) {
       return console.log(err);
     }
   }
   else if(control == 'None'){
     try {
-      const res = await pool.query('SELECT DISTINCT INSTNM FROM university WHERE CREDDESC = $1', [course]);
-      return res.rows;
+      const res = await pool`SELECT DISTINCT INSTNM FROM university WHERE CREDDESC = ${course};`
+      return res ;
     } catch (err) {
       return console.log(err);
     }
   }
   else{
     try {
-      const res_1 = await pool.query('SELECT DISTINCT INSTNM FROM university WHERE CONTROL = $1', [control]);
-      return res_1.rows;
+      const res_1 = await pool`SELECT DISTINCT INSTNM FROM university WHERE CONTROL = ${control};`
+      return res_1 ;
     } catch (err_1) {
       return console.log(err_1);
     }
@@ -72,8 +87,8 @@ async function getunis(control,course){
 
 async function getinstData(instnm){
   try {
-    const res = await pool.query('SELECT DISTINCT CREDDESC FROM university WHERE INSTNM = $1', [instnm]);
-    return res.rows;
+    const res = await pool`'SELECT DISTINCT CREDDESC FROM university WHERE INSTNM = $1', [instnm]`;
+    return res ;
   } catch (err) {
     return console.log(err);
   }
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const mainRouter = require('./routes/main.route');
+
 port = 4800;
 app.use(cors());
 app.use('/v1',mainRouter);
